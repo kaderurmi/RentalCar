8dc913daa0156f2109071edf4f0de3fdb0515f38
Md Fazlul Kader <kadeurmi@gmail.com>
2024-09-27 22:16:20 +0600

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.myapp')
+@section('content')
+    <div class="flex flex-col justify-center items-center gap-12 my-8 mx-auto max-w-screen-xl ">
+  
+        <h2 class="text-4xl font-extrabold tracking-tight text-center text-gray-900 font-car">About
+            Us</h2>
+<h3 class=" font-bold text-center text-blue-700  font-car sm:text-xl">EASY AND FAST WAY TO RENT YOUR CAR
+ </h3>
+        <div class=" text-start flex flex-col  gap-8 w-2/3">
+            <p>
+                RentalCar ("we," "us," or "our") operates the RentalCar website and app
+                ("Platform"). This how we collect, use, and disclose personal information from users
+                of
+                the Platform. By accessing and using our Platform, you consent to the practices described in this Privacy
+                Policy.
+            </p>
+
+            <h3 class="text-xl font-medium">
+                Information Collection and Use </h3>
+            <p class=" ms-4">
+                a. Personal Information: We may collect personal information from users, including but not limited to name,
+                address, phone number, email address, driving license details, payment information, and other information
+                required to process car rental Booking and transactions.
+            </p>
+
+            <p class=" ms-4">
+                b. Usage Data: We automatically collect certain information about your device, such as IP address, browser
+                type,
+                operating system, and browsing activity, to improve our Platform and user experience.
+            </p>
+
+            <p class=" ms-4">
+                c. Cookies and Tracking Technologies: We use cookies and similar technologies to track user interactions on
+                the
+                Platform and provide personalized content.
+            </p>
+
+
+
+            <h3 class="text-xl font-medium">
+                Data Security: </h3>
+            <p class=" ms-4">
+                We implement reasonable security measures to protect your personal information from unauthorized access,
+                disclosure, alteration, or destruction. However, no data transmission over the internet or electronic
+                storage
+                method is entirely secure, and we cannot guarantee absolute data security.
+            </p>
+
+            <h3 class="text-xl font-medium">
+                Data Retention</h3>
+            <p class=" ms-4">
+                We will retain your personal information for as long as necessary to fulfill the purposes outlined in this
+                About Us and comply with legal requirements.
+            </p>
+
+            <h3 class="text-xl font-medium">
+                Third-Party Services</h3>
+            <p class=" ms-4">
+                We may use third-party services, such as payment processors and analytics providers, to facilitate Platform
+                operations. These third-party services have their own privacy policies and terms of use, which are separate
+                from
+                ours. We recommend reviewing their policies before interacting with these services.
+            </p>
+            <h3 class="text-xl font-medium">
+
+                User Rights</h3>
+            <p class=" ms-4">
+                You have the right to access, update, correct, or delete your personal information. If you wish to exercise
+                these rights, please contact us using the contact information provided at the end of this About Us.
+            </p>
+        </div>
+
+
+    </div>
+@endsection
